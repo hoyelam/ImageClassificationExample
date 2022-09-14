eea282d567a9287860c9817f64a6a54fc525c846
Hoye Lam <hoyelam@protonmail.com>
2022-09-14 20:31:50 +0200

    Initial Commit

---
+++ ImageClassificationExample/ImageClassificationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageClassificationExampleApp.swift
+//  ImageClassificationExample
+//
+//  Created by Hoye Lam on 14/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct ImageClassificationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
